8944debaec7fdbffef0099eaaf2cbb95e08cb6ed
Shrey Rupani <ShreyRupani@users.noreply.github.com>
2015-08-05 22:58:34 +0530

    Updated Osmos.c

--- Osmos.c
+++ Osmos.c
@@ -1,17 +1,128 @@
+
+Conversation opened. 1 read message.
+
+Skip to content
+Using Gmail with screen readers
+Shrey
+ 
+ 
+More 
+1 of 11
+ 
+Osmos
+Inbox
+	x
+Ritvik Gautam
+	
+Attachments10:53 PM (0 minutes ago)
+		
+to me
+--
+Ritvik Gautam
+Attachments area
+Preview attachment Osmos.c
+[C]
+	
+Click here to Reply or Forward
+3.75 GB (24%) of 15 GB used
+Manage
+Terms - Privacy
+Last account activity: in 2 minutes
+Details
+	
+	
+Ritvik Gautam's profile photo
+	Ritvik Gautam
+Show details
+Osmos.c
+
 #include<GL/glut.h>
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 10
+#include<math.h>
+#include<sys/time.h>
+#define MAX 30
 
+struct timeval begin, end, diff;
+
+GLfloat size = 40;
+GLfloat sizeOther[MAX];
 //Player square position. Order : LEFT TOP RIGHT BOTTOM
-GLint player[4] = {350,400,400,350};
+GLint player[4];
 //All other squares position AND direction. Yet to be initialised.
 GLint others[MAX][5];
+
+GLint flag = 0;
+
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+{
+    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+    result->tv_sec = diff / 1000000;
+    result->tv_usec = diff % 1000000;
+
+    return (diff<0);
+}
+
+void CollisionHandler(int i)
+{
+    //sleep(1);
+
+    if(sizeOther[i] > size)
+    {
+        size = size - (0.02*sizeOther[i]);
+        player[2] = player[0]+size;
+        player[1] = player[3]+size;
+
+
+        sizeOther[i] = sizeOther[i] + (0.02*sizeOther[i]);
+
+        others[i][2] = others[i][0] + sizeOther[i];
+        others[i][3] = others[i][1] - sizeOther[i];
+    }
+    else
+    {
+        size = size + (0.02*sizeOther[i]);
+        player[2] = player[0]+size;
+        player[1] = player[3]+size;
+
+        sizeOther[i] = sizeOther[i] - (0.2*sizeOther[i]);
+
+        others[i][2] = others[i][0] + sizeOther[i];
+        others[i][3] = others[i][1] - sizeOther[i];
+	}
+
+}
+
+void CollisionDetection()
+{
+    int i, flag = 0;
+    for(i = 0; i < MAX; i++)
+    {
+        if(((others[i][0] >= player[0]) && (others[i][0] <= player[2])) && (((others[i][3] > player[3]) && (others[i][3]) < player[1]) || ((others[i][1] < player[1]) && (others[i][1] > player[3]))))
+        {
+            CollisionHandler(i);
+        }
+        else if(((others[i][3] <= player[1]) && (others[i][3] >= player[3])) && (((others[i][0] > player[0]) && (others[i][0]) < player[2]) || ((others[i][2] < player[2]) && (others[i][2] > player[0]))))
+        {
+            CollisionHandler(i);
+        }
+        else if(((others[i][0] <= player[2]) && (others[i][0] >= player[0])) && (((others[i][1] > player[3]) && (others[i][1]) < player[1]) || ((others[i][3] < player[1]) && (others[i][3] > player[3]))))
+        {
+            CollisionHandler(i);
+        }
+        else if(((others[i][1] >= player[3]) && (others[i][1] <= player[1])) && (((others[i][0] > player[0]) && (others[i][0]) < player[2]) || ((others[i][2] < player[2]) && (others[i][2] > player[0]))))
+        {
+            CollisionHandler(i);
+        }
+    }
+}
+
 void myinit()
 {
-
 	glClearColor(1.0,1.0,1.0,1.0);
 	glColor3f(1.0,0.0,0.0);
+
+    gettimeofday(&begin, NULL);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -35,50 +146,16 @@ void DrawPlayer()
 
 void ModifyPlayer(int x, int y)
 {
-	player[0] += x;
-	player[2] += x;
-	player[1] += y;
-	player[3] += y;
+	player[0] = x;
+	player[2] = x+size;
+	player[1] = y+size;
+	player[3] = y;
 }
-
-void MovePlayer(unsigned char key, int x, int y)
+void MovePlayer(int x, int y)
 {
-	switch(key)
-	{
-		case '4':
-		case 'a':
-			ModifyPlayer(-20,0);
-			break;
-		case '6':
-		case 'd':
-			ModifyPlayer(20,0);
-			break;
-		case '8':
-		case 'w':
-			ModifyPlayer(0,20);
-			break;
-		case '2':
-		case 's':
-			ModifyPlayer(0,-20);
-			break;
-		case '7':
-			ModifyPlayer(-20,20);
-			break;
-		case '9':
-			ModifyPlayer(20,20);
-			break;
-		case '1':
-			ModifyPlayer(-20,-20);
-			break;
-		case '3':
-			ModifyPlayer(20,-20);
-			break;
-
-		default:
-			break;
-	}
+    y=728-abs(y);
+    ModifyPlayer(x,y);
 }
-
 void InitialiseOthers()
 {
 	int i, sizebox;
@@ -90,15 +167,22 @@ void InitialiseOthers()
 		others[i][2] = others[i][0] + sizebox;
 		others[i][3] = others[i][1] - sizebox;
 		others[i][4] = rand() % 8;
+		sizeOther[i] = (float)sizebox;
 	}
 }
+
 void DrawOthers()
 {
 	int i;
 	glPointSize(2.0);
-	glColor3f(0.258824,0.258824,0.435294);
+	//glColor3f(0.258824,0.258824,0.435294);
+
 	for(i = 0; i < MAX; i++)
 	{
+        if(sizeOther[i] > size+1)
+            glColor3f(1.0,0.0,0.0);
+        else if(sizeOther[i] <= size-1)
+            glColor3f(0.0,1.0,0.0);
 		glBegin(GL_QUADS);
 			glVertex2f(others[i][0], others[i][3]);
 			glVertex2f(others[i][0], others[i][1]);
@@ -110,7 +194,7 @@ void DrawOthers()
 
 void ModifyOthers()
 {
-	float x, y, speed = 1;
+	float x, y, speed = 5;
 	int i;
 	for(i = 0; i < MAX; i++)
 	{
@@ -157,7 +241,7 @@ void ModifyOthers()
 		others[i][3] += y;
 
 //Bounce off LEFT wall
-		if(others[i][0] == 0)
+		if(others[i][0] < 0)
 		{
 			switch(others[i][4])
 			{
@@ -175,7 +259,7 @@ void ModifyOthers()
 			}
 		}
 //Bounce off TOP wall
-		if(others[i][1] == 768)
+		if(others[i][1] > 768)
 		{
 			switch(others[i][4])
 			{
@@ -193,7 +277,7 @@ void ModifyOthers()
 			}
 		}
 //Bounce off RIGHT wall
-		if(others[i][2] == 1366)
+		if(others[i][2] > 1366)
 		{
 			switch(others[i][4])
 			{
@@ -211,7 +295,7 @@ void ModifyOthers()
 			}
 		}
 //Bounce off BOTTOM wall
-		if(others[i][3] == 0)
+		if(others[i][3] < 0)
 		{
 			switch(others[i][4])
 			{
@@ -232,7 +316,60 @@ void ModifyOthers()
 //	display();
 }
 
+void GameWinDetection()
+{
+    int i, temp = 0;
+    for(i = 0; i < MAX; i++)
+    {
+        if(sizeOther[i] < size)
+            temp++;
+    }
+    if(temp == MAX)
+    {
+        flag = 1;
+        GameOver(500, 400, "CONGRATULATIONS! YOU WIN!");
+    }
+}
 
+void GameOver(int x, int y, char *string)
+{
+    glClear(GL_DEPTH_BUFFER_BIT);
+  glColor3f(0,0,0);
+  glRasterPos2f(x, y);
+  int len, i;
+  len = (int)strlen(string);
+  for (i = 0; i < len; i++)
+  {
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, string[i]);
+  }
+  gettimeofday(&end, NULL);
+  timeval_subtract(&diff, &end, &begin);
+  if(flag == 1)
+  {
+    //printf("You score: %ld.%02ld!\n", diff.tv_sec, diff.tv_usec);
+     printf("You score: %d!\n", 100-(int)diff.tv_sec);
+    }
+    else
+    {
+        printf("You Lose!\n");
+    }
+  glutIdleFunc(NULL);
+}
+
+void GameLoseDetection()
+{
+    int i, temp = 0;
+    for(i = 0; i < MAX; i++)
+    {
+        if(size < sizeOther[i])
+            temp++;
+    }
+    if(temp == MAX || size < 5)
+    {
+        flag = 0;
+        GameOver(500, 400, "GAME OVER! BETTER LUCK NEXT TIME");
+    }
+}
 
 void display()
 {
@@ -241,6 +378,10 @@ void display()
 	DrawOthers();
 	glFlush();
 	ModifyOthers();
+	CollisionDetection();
+	GameWinDetection();
+	GameLoseDetection();
+	glutSwapBuffers();
 }
 
 
@@ -248,14 +389,18 @@ void display()
 int main(int argc, char **argv)
 {
 	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_DOUBLE);
 	glutInitWindowSize(1366,768);
 	glutInitWindowPosition(100,100);
 	glutCreateWindow("Osmos");
 	glutDisplayFunc(display);
-	glutKeyboardFunc(MovePlayer);
+	glutSetCursor(GLUT_CURSOR_NONE);
+	glutPassiveMotionFunc(MovePlayer);
+	//glutKeyboardFunc(MovePlayer);
 	myinit();
 	glutIdleFunc(display);
 	glutMainLoop();
 	return 0;
 }
+
+Displaying Osmos.c.
